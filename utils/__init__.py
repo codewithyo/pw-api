e325bc77ffdef7d5a387cad204fdc2fb621a3c90
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-05-12 13:08:03 +0530

    Rename and remove files

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,10 +1,14 @@
 from json import load
 from pathlib import Path
+from typing import TypeAlias
 
 from utils.streamlit.analytics import (AnalyticsSubmissions, AnalyticsUsers,
                                        QuizAnalytics, get_live_course_df)
 from utils.streamlit.live_course import LiveCourse
 from utils.streamlit.preview_course import PreviewCourse
 
-courses_dict_fp = Path('data/courses/all_courses_dict.json')
-courses_dict: dict[str, str] = load(open(courses_dict_fp))
+CourseName: TypeAlias = str
+CourseId: TypeAlias = str
+
+courses_dict_fp = Path('data/all_courses_dict.json')
+courses_dict: dict[CourseId, CourseName] = load(open(courses_dict_fp))
